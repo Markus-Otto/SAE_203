610a3bb3a3ff199aa26846945937818ce9d12566
jorisbrusa <joris.brusa@gmail.com>
2024-06-03 12:08:42 +0200

    second commit

--- connection.php
+++ connection.php
@@ -9,33 +9,21 @@ $conn = connexion();
 $login = $_POST['login'];
 $pass = $_POST['password'];
 
-echo $login;
-echo $pass;
-
-
-
 // Préparer et exécuter la requête SQL
 $sql = $conn->prepare("SELECT username, pass FROM users WHERE username = ? AND pass = ? ");
 if ($sql === false) {
     die("Erreur de préparation de la requête: " . $conn->error);
 }
 $hashed_password=md5($pass);
-echo $hashed_password.'<br>';
+
 $sql->bind_param('ss', $login,$hashed_password );
 $sql->execute();
 $sql->store_result();
 
-// Vérifier si l'utilisateur existe
-echo $sql->num_rows;
 if ($sql->num_rows > 0) {
     $sql->bind_result( $hashed_password, $username);
     $sql->fetch();
 
-    // Debugging: Afficher les valeurs récupérées
-    echo "Hashed Password: " . $hashed_password . "<br>";
-
-    // Vérifier le mot de passe
-    echo $pass.'<br>';
     if (!password_verify($pass, $hashed_password)) {
         // Initialiser la session
     
@@ -48,6 +36,11 @@ if ($sql->num_rows > 0) {
     }
 } else  {
     echo "Identifiant incorrect.";
+    echo $login. "<br>";
+    echo "Hashed Password: " . $hashed_password . "<br>";
+    echo $pass.'<br>';
+    echo $sql->num_rows.'<br>';
+    echo $hashed_password.'<br>';
 }
 
 $sql->close();
